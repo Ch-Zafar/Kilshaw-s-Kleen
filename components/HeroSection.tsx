77238a9a25ceeac4ce16b6dd29255401ca4b786b
Rafaz Zafar <chaudaryzafar279@gmail.com>
2026-08-14 15:34:57 +0500

    Initial Commit

---
+++ components/HeroSection.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "./Navbar";
+
+export default function HeroSection() {
+    return (
+        <div className="w-full h-screen bg-black">
+            <Navbar />
+        </div>
+    );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export function Navbar(){
+    return(
+        <div className="w-full h-32 bg-transparent flex flex-row justify-between">
+            <div className="w-1/3 h-full">
+                <Image src={"/Images/Logo.png"} alt="Image Not found" width={250} height={250}/>
+            </div>
+            <div className="w-1/3 h-full">
+                <ul className="w-full h-full flex flex-row justify-evenly items-center ">
+                    <li><a href="#">Solar Panel Cleaning</a></li>
+                    <li><a href="#">AC Condeser Cleaning</a></li>
+                    <li><a href="#">About</a></li>
+
+                </ul>
+            </div>
+            <div className="w-1/3 h-full flex flex-row justify-end">
+                <button className="w-36 h-16 bg-white rounded-4xl text-black">
+                    Contact Us!
+                </button>
+            </div>
+        </div>
+    );
+
+
+}
