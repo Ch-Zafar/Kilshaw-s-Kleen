fd67ee75a7e58613a8abe2d95bb0b42c377bfab4
Rafaz Zafar <chaudaryzafar279@gmail.com>
2026-08-15 18:10:04 +0500

    commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 export function Navbar(){
     return(
         <div className="w-full h-32 bg-transparent flex flex-row justify-between">
-            <div className="w-1/3 h-full">
-                <Image src={"/Images/Logo.png"} alt="Image Not found" width={250} height={250}/>
+            <div className="w-1/4 h-full">
+                <Image src={"/Images/Logo.png"} alt="Image Not found" width={150} height={150}/>
             </div>
-            <div className="w-1/3 h-full">
-                <ul className="w-full h-full flex flex-row justify-evenly items-center ">
+            <div className="w-1/2 h-full">
+                <ul className="w-full h-full flex flex-row justify-between items-center ">
                     <li><a href="#">Solar Panel Cleaning</a></li>
                     <li><a href="#">AC Condeser Cleaning</a></li>
                     <li><a href="#">About</a></li>
 
                 </ul>
             </div>
-            <div className="w-1/3 h-full flex flex-row justify-end">
-                <button className="w-36 h-16 bg-white rounded-4xl text-black">
+            <div className="w-1/4 h-full flex flex-row justify-end items-center">
+                <button className="w-36 h-10 bg-white rounded-4xl text-black">
                     Contact Us!
                 </button>
             </div>
