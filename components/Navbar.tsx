ace5b0c2c044b5cac3fbe8de76a45df946eaf21d
Zafar <gzafar4879@gmail.com>
2026-08-15 12:38:04 +0500

    Second commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
+import Button from "./Button";
 
 export function Navbar(){
     return(
-        <div className="w-full h-32 bg-transparent flex flex-row justify-between">
-            <div className="w-1/3 h-full">
-                <Image src={"/Images/Logo.png"} alt="Image Not found" width={250} height={250}/>
+        <div className="w-full h-16 bg-transparent flex flex-row justify-between ">
+            <div className="w-1/4 h-full flex flex-row items-center">
+                <Image src={"/Images/Logo.png"} alt="Image Not found" width={100} height={100}/>
             </div>
-            <div className="w-1/3 h-full">
-                <ul className="w-full h-full flex flex-row justify-evenly items-center ">
+            <div className="w-1/2 h-full">
+                <ul className="w-full h-full flex flex-row justify-between items-center ">
                     <li><a href="#">Solar Panel Cleaning</a></li>
                     <li><a href="#">AC Condeser Cleaning</a></li>
                     <li><a href="#">About</a></li>
 
                 </ul>
             </div>
-            <div className="w-1/3 h-full flex flex-row justify-end">
-                <button className="w-36 h-16 bg-white rounded-4xl text-black">
-                    Contact Us!
-                </button>
+            <div className="w-1/4 h-full flex flex-row justify-end items-center">
+                <Button title="Contact Me"/>
             </div>
         </div>
     );
---
+++ components/Services.tsx
@@ -0,0 +1,17 @@
+export default function Services() {
+    return (
+        <div className="w-full h-screen flex flex-col items-center ">
+            <h2 className="text-5xl ">Our Services</h2>
+            <div className="w-full h-10/12 flex">
+
+                <div className="w-1/2 h-full ">
+
+                </div>
+
+                <div className="w-1/2 h-full ">
+
+                </div>
+            </div>
+        </div>
+    )
+}
